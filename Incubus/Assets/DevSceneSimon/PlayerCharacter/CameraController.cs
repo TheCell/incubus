6e5490464e1b6ebebd7c112422faa3404ceff7fb
TheCell <simon_hischier@hotmail.com>
2019-02-19 16:40:15 +0100

    added first player movement

---
+++ Incubus/Assets/DevSceneSimon/PlayerCharacter/CameraController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Renaissance Coders Tutorial helped here
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] private Transform target;
+	[SerializeField] private float lookSmooth = 0.09f;
+	[SerializeField] private Vector3 offsetFromTarget = new Vector3(0, 2, -8);
+	[SerializeField] private float xTilt = 10;
+
+	private Vector3 destination = Vector3.zero;
+	private PlayerCharacterController playerCharacterController;
+	private float rotateVelocity = 0;
+
+	private void Start()
+    {
+		SetCameraTarget(target);
+    }
+
+	private void SetCameraTarget(Transform targetTransform)
+	{
+		target = targetTransform;
+
+		if (target != null)
+		{
+			if (target.GetComponent<PlayerCharacterController>())
+			{
+				playerCharacterController = target.GetComponent<PlayerCharacterController>();
+			}
+			else
+			{
+				Debug.LogError("The camera's target needs a player character controller.");
+			}
+		}
+		else
+		{
+			Debug.LogError("Your camera needs a target");
+		}
+	}
+
+	private void LateUpdate()
+	{
+		// moving
+		MoveToTarget();
+		// rotate
+		LookAtTarget();
+	}
+
+	private void MoveToTarget()
+	{
+		destination = playerCharacterController.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	private void LookAtTarget()
+	{
+		float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVelocity, lookSmooth);
+		//float eulerXAngle = transform.eulerAngles.x;
+		float eulerXAngle = xTilt;
+
+		transform.rotation = Quaternion.Euler(eulerXAngle, eulerYAngle, 0.0f);
+	}
+}
---
+++ Incubus/Assets/DevSceneSimon/PlayerCharacter/PlayerCharacterController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Renaissance Coders Tutorial helped here
+public class PlayerCharacterController : MonoBehaviour
+{
+	[System.Serializable]
+	public class MoveSettings : System.Object
+	{
+		public float forwardVelocity = 12;
+		public float rotationVelocity = 100;
+		public float jumpVelocity = 25;
+		public float distToGrounded = 0.78f;
+		public LayerMask ground;
+	}
+
+	[System.Serializable]
+	public class PhysicsSettings : System.Object
+	{
+		public float downAcceleration = 0.75f;
+	}
+
+	[System.Serializable]
+	public class InputSettings : System.Object
+	{
+		public float inputDelay = 0.1f;
+		public string FORWARD_AXIS = "Vertical";
+		public string TURN_AXIS = "Horizontal";
+		public string JUMP_AXIS = "Jump";
+	}
+
+	[SerializeField] private MoveSettings moveSettings = new MoveSettings();
+	[SerializeField] private PhysicsSettings physicsSettings = new PhysicsSettings();
+	[SerializeField] private InputSettings inputSettings = new InputSettings();
+
+	private Vector3 velocity = Vector3.zero;
+	private Quaternion targetRotation;
+	private Rigidbody rigidb;
+	private float forwardInput, turnInput, jumpInput;
+
+	public Quaternion TargetRotation
+	{
+		get { return targetRotation; }
+	}
+
+	private bool IsGrounded()
+	{
+		Debug.DrawRay(transform.position, Vector3.down);
+		return Physics.Raycast(transform.position, Vector3.down, moveSettings.distToGrounded, moveSettings.ground);
+	}
+
+	private void Start()
+	{
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody>())
+		{
+			rigidb = GetComponent<Rigidbody>();
+		}
+		else
+		{
+			Debug.LogError("The Character needs a rigidbody");
+		}
+
+		forwardInput = turnInput = jumpInput = 0;
+	}
+
+	private void GetInput()
+	{
+		forwardInput = Input.GetAxis(inputSettings.FORWARD_AXIS); // interpolated
+		turnInput = Input.GetAxis(inputSettings.TURN_AXIS); // interpolated
+		jumpInput = Input.GetAxisRaw(inputSettings.JUMP_AXIS); // non-interpolated
+	}
+
+	private void Update()
+	{
+		GetInput();
+		Turn();
+	}
+
+	private void FixedUpdate()
+	{
+		Run();
+		Jump();
+
+		rigidb.velocity = transform.TransformDirection(velocity);
+	}
+
+	private void Run()
+	{
+		if (Mathf.Abs(forwardInput) > inputSettings.inputDelay)
+		{
+			// move
+			velocity.z = moveSettings.forwardVelocity * forwardInput;
+		}
+		else
+		{
+			// zero velocity
+			velocity.z = 0;
+		}
+	}
+
+	private void Turn()
+	{
+		if (Mathf.Abs(turnInput) > inputSettings.inputDelay)
+		{
+			targetRotation *= Quaternion.AngleAxis(moveSettings.rotationVelocity * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+	}
+
+	private void Jump()
+	{
+		Debug.Log(IsGrounded());
+
+		if (jumpInput > 0 && IsGrounded())
+		{
+			velocity.y = moveSettings.jumpVelocity;
+		}
+		else if (jumpInput == 0 && IsGrounded())
+		{
+			// zero out our velocity.y
+			velocity.y = 0;
+		}
+		else
+		{
+			// decrease velocity.y
+			velocity.y -= physicsSettings.downAcceleration;
+		}
+	}
+}
