d84dc0fdd7d8d8f70388710799d3805ab5f73868
TheCell <simon_hischier@hotmail.com>
2019-03-07 10:55:33 +0100

    added Platforms

---
+++ Incubus/Assets/Scenes/InteractionDev/InteractionDevScript/MovingPlatform.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+	[SerializeField] private GameObject startPoint;
+	[SerializeField] private GameObject endPoint;
+	[SerializeField] private float durationSeconds = 2f;
+	[SerializeField] private bool inverseDirection = false;
+
+	private Rigidbody platformRB;
+	private bool reverseDirection = false;
+	private float timeSincePlatformStart;
+	private float currentPosition = 0f;
+	private List<Rigidbody> collidingBodies = new List<Rigidbody>();
+	private Vector3 positionDelta = Vector3.zero;
+
+	private void Start()
+	{
+		platformRB = GetComponent<Rigidbody>();
+
+		if (platformRB == null)
+		{
+			Debug.LogError("Platform needs a Rigidbody");
+		}
+
+		reverseDirection = inverseDirection;
+		timeSincePlatformStart = Time.time;
+	}
+
+	private void FixedUpdate()
+	{
+		UpdateCurrentPosition();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		collidingBodies.Add(collision.rigidbody);
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		collidingBodies.Remove(collision.rigidbody);
+	}
+
+	private void UpdateCurrentPosition()
+	{
+		UpdateDirectionAndTime();
+		float relativeTime = Time.time - timeSincePlatformStart;
+
+		if (reverseDirection)
+		{
+			currentPosition = 1 - (1 / durationSeconds * relativeTime);
+		}
+		else
+		{
+			currentPosition = 1 / durationSeconds * relativeTime;
+		}
+
+		Vector3 newPosition = Vector3.Lerp(startPoint.transform.position, endPoint.transform.position, currentPosition);
+		positionDelta = newPosition - transform.position;
+		platformRB.MovePosition(newPosition);
+
+		foreach(Rigidbody rb in collidingBodies)
+		{
+			rb.MovePosition(rb.position + positionDelta);
+		}
+	}
+
+	private void UpdateDirectionAndTime()
+	{
+		if (timeSincePlatformStart + durationSeconds < Time.time)
+		{
+			timeSincePlatformStart = Time.time;
+			reverseDirection = !reverseDirection;
+		}
+	}
+}
