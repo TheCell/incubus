7789b1f58538df579e61497ef9d9a8884005597b
TheCell <simon_hischier@hotmail.com>
2019-04-10 14:59:59 +0200

    added dynamic loading system

---
+++ Incubus/Assets/Scenes/LoadSystem/AdditionalSceneLoader.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdditionalSceneLoader : MonoBehaviour
+{
+	private List<string> currentScenes = new List<string>();
+	private List<string> scenesToLoad = new List<string>();
+	private List<string> scenesToUnload = new List<string>();
+	private Scene rootScene;
+
+	private void Start()
+	{
+		rootScene = SceneManager.GetActiveScene();
+	}
+
+	public void LoadScenes(SceneInformation sceneInformation)
+	{
+		UpdateScenesToLoad(sceneInformation);
+		LoadScenes();
+
+		UpdateScenesToUnload(sceneInformation);
+		UnloadScenes();
+	}
+
+	private void LoadScene(string sceneName)
+	{
+		if (!sceneName.Equals(rootScene.name))
+		{
+			SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+		}
+	}
+
+	private void LoadScenes()
+	{
+		scenesToLoad.ForEach(item =>
+		{
+			LoadScene(item);
+		});
+	}
+
+	private void UnloadScenes()
+	{
+		scenesToUnload.ForEach(item =>
+		{
+			UnloadScene(item);
+		});
+	}
+
+	private void UnloadScene(string sceneName)
+	{
+		if (!sceneName.Equals(rootScene.name))
+		{
+			SceneManager.UnloadSceneAsync(sceneName);
+		}
+	}
+
+	private void UpdateScenesToLoad(SceneInformation sceneInfo)
+	{
+		string[] shouldBeScenes = sceneInfo.GetShouldBeActiveScenes();
+		List<string> newScenes = new List<string>();
+		
+		for (int i = 0; i < shouldBeScenes.Length; i++)
+		{
+			string sceneName = shouldBeScenes[i];
+			if (!currentScenes.Contains(sceneName))
+			{
+				newScenes.Add(sceneName);
+			}
+		}
+
+		scenesToLoad = newScenes;
+		AddToCurrentScenes(newScenes);
+	}
+
+	private void AddToCurrentScenes(List<string> newScenes)
+	{
+		newScenes.ForEach(item =>
+		{
+			if (!currentScenes.Contains(item))
+			{
+				currentScenes.Add(item);
+			}
+		});
+	}
+	
+	private void UpdateScenesToUnload(SceneInformation sceneInfo)
+	{
+		List<string> relevantScenes = new List<string>(sceneInfo.GetShouldBeActiveScenes());
+		List<string> newScenesToUnload = new List<string>();
+
+		currentScenes.ForEach(item =>
+		{
+			if (!relevantScenes.Contains(item))
+			{
+				newScenesToUnload.Add(item);
+			}
+		});
+		
+		scenesToUnload = newScenesToUnload;
+
+		scenesToUnload.ForEach(item =>
+		{
+			currentScenes.Remove(item);
+		});
+	}
+}
---
+++ Incubus/Assets/Scenes/LoadSystem/SceneInformation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneInformation : MonoBehaviour
+{
+	[SerializeField] private string[] activeScenes;
+
+	public string[] GetShouldBeActiveScenes()
+	{
+		return activeScenes;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		AdditionalSceneLoader sceneLoader = other.GetComponent<AdditionalSceneLoader>();
+		if (sceneLoader == null)
+		{
+			return;
+		}
+
+		sceneLoader.LoadScenes(this);
+	}
+}
