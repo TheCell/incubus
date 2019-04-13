33fb901b64b2a790b88eb98153fe46e1493fbb37
TheCell <simon_hischier@hotmail.com>
2019-04-13 14:10:15 +0200

    added tower logic

---
+++ Incubus/Assets/Scenes/InteractionDev/FinalTowerSystem/TowerTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTrigger : MonoBehaviour
+{
+	[SerializeField] private Towerlogic towerScript;
+	private bool wasActivated;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (wasActivated)
+		{
+			return;
+		}
+
+		if (other.tag == "Player")
+		{
+			wasActivated = true;
+			towerScript.RemovePart();
+		}
+	}
+}
---
+++ Incubus/Assets/Scenes/InteractionDev/FinalTowerSystem/Towerlogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Towerlogic : MonoBehaviour
+{
+	[SerializeField] private List<GameObject> towerParts;
+
+	private void Start()
+	{
+		towerParts.Reverse();
+	}
+
+	public void RemovePart()
+	{
+		IEnumerator<GameObject> partEnumerator = towerParts.GetEnumerator();
+		if (partEnumerator.MoveNext())
+		{
+			GameObject element = partEnumerator.Current;
+			towerParts.Remove(element);
+			Destroy(element);
+		}
+	}
+}
