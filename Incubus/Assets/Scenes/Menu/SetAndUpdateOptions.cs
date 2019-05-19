3e63d252a2d0dc71a2f2508f7d72df95baefaed3
TheCell <simon_hischier@hotmail.com>
2019-05-19 19:22:55 +0200

    added functioning CameraSpeed and camera Y invert

---
+++ Incubus/Assets/Scenes/Menu/SetAndUpdateOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetAndUpdateOptions : MonoBehaviour
+{
+	public Slider cameraSpeedSlider;
+	public Toggle invertCamera;
+
+	public void SetCameraInvertBool(bool value)
+	{
+		PlayerCamera.InvertCamera = value;
+	}
+
+	public void SetCameraSpeed(float value)
+	{
+		PlayerCamera.CameraMultipier = value;
+	}
+
+	void Start()
+	{
+		cameraSpeedSlider.value = PlayerCamera.CameraMultipier;
+		invertCamera.isOn = PlayerCamera.InvertCamera;
+	}
+}
