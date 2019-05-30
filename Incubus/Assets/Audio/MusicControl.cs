a1253b211f6a97d0d21b2bb1759959a038121f8c
michael schürmann <michaelschuermann@hotmail.com>
2019-05-30 23:01:07 +0200

    Add new Sound Logic

---
+++ Incubus/Assets/Audio/MusicControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControl : MonoBehaviour {
+
+    public AudioMixerSnapshot outOfCompbat;
+    public AudioMixerSnapshot inCombat;
+    public AudioClip[] stings;
+    public AudioSource stingSource;
+    public float bpm = 128;
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote * 32;
+
+            
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Sound"))
+        {
+            inCombat.TransitionTo(m_TransitionIn);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Sound"))
+        {
+            outOfCompbat.TransitionTo(m_TransitionOut);
+        
+            }
+    }
+}
---
+++ Incubus/Assets/Audio/MusicControl1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControl1 : MonoBehaviour {
+
+    public AudioMixerSnapshot outOfCompbat2;
+    public AudioMixerSnapshot inCombat2;
+    public AudioClip[] stings;
+    public AudioSource stingSource;
+    public float bpm = 128;
+
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote * 32;
+
+            
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Sound2"))
+        {
+            inCombat2.TransitionTo(m_TransitionIn);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Sound2"))
+        {
+            outOfCompbat2.TransitionTo(m_TransitionOut);
+        
+            }
+    }
+}
