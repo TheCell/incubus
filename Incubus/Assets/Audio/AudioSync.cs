0135d17ebd47261cbf4970bc9e8ddcc2652c6776
TheCell <simon_hischier@hotmail.com>
2019-06-02 14:53:49 +0200

    Audio Fix

---
+++ Incubus/Assets/Audio/AudioSync.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSync : MonoBehaviour
+{
+    //set these in the inspector!
+    public AudioSource master;
+    public AudioSource slave;
+
+    void Update()
+    {
+        slave.timeSamples = master.timeSamples;
+    }
+}
