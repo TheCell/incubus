9a7b41083ceac24a5b6b1ab4d4139b3193ce8b9c
TheCell <simon_hischier@hotmail.com>
2019-06-11 14:01:29 +0200

    Sound Script Update

---
+++ Incubus/Assets/Audio/AudioSync2.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSync2 : MonoBehaviour
+{
+
+
+    //set these in the inspector!
+    private AudioSource master;
+    public AudioSource[] slaves;
+
+    private void Start()
+    {
+        master = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        foreach( AudioSource slave in slaves)
+        {
+            slave.timeSamples = master.timeSamples;
+        }
+    }
+
+    private IEnumerator SyncSources()
+    {
+        while (true)
+        {
+            foreach (var slave in slaves)
+            {
+                slave.timeSamples = master.timeSamples;
+                yield return null;
+            }
+        }
+    }
+}
