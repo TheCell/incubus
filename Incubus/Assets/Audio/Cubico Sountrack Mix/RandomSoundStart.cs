078dd477fa7c7f4b4245fefe9c6a39abc52d5d40
TheCell <simon_hischier@hotmail.com>
2019-11-14 10:27:14 +0100

    fixed last table, added random sound start

---
+++ Incubus/Assets/Audio/Cubico Sountrack Mix/RandomSoundStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundStart : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(0, targetVolume, currentTime / duration);
+            yield return null;
+        }
+
+        yield break;
+    }
+
+    private void Start()
+    {
+        float audiolength = musicSource.clip.length;
+        musicSource.time = Random.Range(0, audiolength);
+        StartCoroutine(StartFade(musicSource, 3f, musicSource.volume));
+    }
+}
